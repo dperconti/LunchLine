35afc5ae7c4542fed5f0e69e4eb020888f02a3a3
Donato Perconti <donato.perconti@gmail.com>
2023-01-18 22:23:09 -0800

    Esqueleto

--- app/Run.hs
+++ app/Run.hs
@@ -1,11 +1,12 @@
 module Run where
 
-import Data.List (foldl')
 import Control.Monad.Logger
 import Control.Monad.Reader
 import Database.Persist.Sqlite
 import Models
 import Transformer.ReaderT (AppT, Env (Env), runAppT, runDB)
+import Data.Maybe (fromMaybe)
+import Database.Esqueleto.Experimental
 
 weeklyBudget :: Int
 weeklyBudget = 100
@@ -13,15 +14,22 @@ weeklyBudget = 100
 spend :: Int -> Entity LineItem -> Int
 spend n e = n - (lineItemAmount $ entityVal e)
 
+getLineItemTotal :: (Num a, MonadIO m, PersistField a) => SqlPersistT m a
+getLineItemTotal = selectSum $ do
+  items <- from $ table @LineItem
+  pure $ sum_ $ items ^. LineItemAmount
+ where
+  selectSum = fmap (maybe 0 (fromMaybe 0 . unValue)) . selectOne
+  -- Note: in Haskell >= 9, due to simplified subsumption, you will need to do replace selectOne with (\q -> selectOne q)
+
 runApp :: AppT ()
 runApp = do
-  lineItems <- runDB $ do
-    runMigration migrateAll
+  total <- runDB $ do
     insert_ $ LineItem "Pizza" 11
     insert_ $ LineItem "Burger" 12
-    selectList [] []
-  let remainingBudget = foldl' spend weeklyBudget lineItems
-  liftIO . putStrLn $ "Remaining Budget: " <> show remainingBudget
+    getLineItemTotal
+  let remainingBudget = weeklyBudget - total
+  liftIO .  putStrLn $ "Remaining Budget: " <> show remainingBudget
 
 run :: IO ()
 run = do
